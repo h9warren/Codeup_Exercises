090ae4f5fe76936623450851ac444d967b1734c0
Hunter Warren <hunter.warren@gmail.com>
2017-07-19 09:43:22 -0500

    Got the do_while done well!

---
+++ do_while.php
@@ -0,0 +1,22 @@
+<?php
+
+// $a = 0;
+// do {
+//   $a+=2;
+//   echo "\$a is equal to {$a}\n";
+// } while ( $a < 100 );
+
+
+// $b = 105;
+// do {
+//   $b-=5;
+//   echo "\$b is equal to {$b}\n";
+// } while ( $b >= -5 );
+
+
+
+$c = 2;
+do {
+  echo "{$c}\n";
+  $c *= $c;
+} while ( $c < 1000000 );
