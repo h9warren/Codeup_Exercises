ee7ed298dc1cdf79ce1cf99aa41a633f8f74b172
Hunter Warren <hunter.warren@gmail.com>
2017-07-20 12:51:25 -0500

    foreach loop now displays value of indices with scalar type only

--- foreach.php
+++ foreach.php
@@ -2,24 +2,30 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
+// foreach ($things as $index) {
+//   if (is_integer($index)){
+//     $type = "integer";
+//   }
+//   if (is_float($index)){
+//     $type = "float";
+//   }
+//   if (is_bool($index)){
+//     $type = "boolean";
+//   }
+//   if (is_array($index)){
+//     $type = "array";
+//   }
+//   if (is_null($index)){
+//     $type = "null";
+//   }
+//   if (is_string($index)){
+//     $type = "string";
+//   }
+//   echo "{$type}\n";
+// }
+
 foreach ($things as $index) {
-  if (is_integer($index)){
-    $type = "integer";
+  if (is_scalar($index)) {
+    echo "{$index}\n";
   }
-  if (is_float($index)){
-    $type = "float";
-  }
-  if (is_bool($index)){
-    $type = "boolean";
-  }
-  if (is_array($index)){
-    $type = "array";
-  }
-  if (is_null($index)){
-    $type = "null";
-  }
-  if (is_string($index)){
-    $type = "string";
-  }
-  echo "{$type}\n";
 }
