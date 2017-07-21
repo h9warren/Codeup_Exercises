e62f4ab45bbc060ccd99bae8c85ea393ecea2733
Hunter Warren <hunter.warren@gmail.com>
2017-07-21 16:14:28 -0500

    it now has a modulus function.

--- arithmetic.php
+++ arithmetic.php
@@ -19,6 +19,10 @@ function divide($a, $b)
 {
     return $a / $b;
 }
+function modulus($a, $b)
+{
+    return $a % $b;
+}
 
 // Add code to test your functions here
 $sum = add(5, 6);
@@ -32,3 +36,6 @@ echo $mult .PHP_EOL;
 
 $quo = divide(81, 9);
 echo $quo .PHP_EOL;
+
+$mod = modulus(31, 2);
+echo $mod .PHP_EOL;
