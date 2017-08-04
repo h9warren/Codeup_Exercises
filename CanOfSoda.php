b3f1897710436da2be4fb770803b8956f57578d5
Hunter Warren <hunter.warren@gmail.com>
2017-08-04 09:44:04 -0500

    morning exercises complete!

--- CanOfSoda.php
+++ CanOfSoda.php
@@ -3,19 +3,27 @@
 class CanOfSoda {
 
   public $brandName;
-  public $isFull;
-  public $isOpen;
+  public $isFull=true;
+  public $isOpen=false;
   public $expirationDate;
 
   public function openSoda() {
-    return $this->brandName . " is open!".PHP_EOL;
-    $this->isOpen=true;
+    if (!$this->isOpen = true) {
+      $this->isOpen = true;
+      return $this->brandName . " is open!".PHP_EOL;
+    } else {
+      return $this->brandName . " is already open!".PHP_EOL;
+    }
   }
+
+
   public function pourOut() {
-    return $this->brandName . " is empty!".PHP_EOL;
-    $this->isFull = false;
+    if ($this->isFull = true) {
+      $this->isFull = false;
+      return $this->brandName . " is empty!".PHP_EOL;
+    } else {
+      return $this->brandName . " is already empty!".PHP_EOL;
+    }
   }
-
 }
-
 ?>
--- CanOfSodaTest.php
+++ CanOfSodaTest.php
@@ -5,13 +5,11 @@ require 'CanOfSoda.php';
   $bigRed = new CanOfSoda ();
 
   $bigRed->brandName='Big Red';
-  $bigRed->isFull=true;
-  $bigRed->isOpen=false;
   $bigRed->expirationDate="01.01.2020";
 
 
   echo $bigRed->openSoda();
   echo $bigRed->pourOut();
-  
+
 
  ?>
